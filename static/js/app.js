b2a2c5ca978e6ec62a19c1d89d416712e210a05c
Kyle Patrick <kylepatrick@Kyles-MBP-2.attlocal.net>
2021-01-14 22:35:54 -0500

    bar charts and init

--- static/js/app.js
+++ static/js/app.js
@@ -1,65 +1,50 @@
-let dropdown = jQuery('#selDataset');
+function buildBarchart(sample){
+  d3.json('samples.json').then((data) => {
+    var samples = data.samples;
+    var resultArray = samples.filter(sampleObj => sampleObj.id == sample);
+    var result = resultArray[0];
 
-const url = 'samples.json';
+    var otu_ids = result.otu_ids;
+    var otu_labels = result.otu_labels;
+    var sample_values = result.sample_values;
 
-// Populate dropdown with list of provinces
-jQuery.getJSON(url, function (data) {
-  jQuery.each(data, function (key, entry) {
-    dropdown.append($('<option></option>').attr('value', entry.name).text(entry.name));
+    var yticks = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
+  
+    // Trace1
+    var data = [
+      {
+      x: sample_values.slice(0,10).reverse(),
+      y: yticks,
+      text: otu_labels.slice(0,10).reverse(),
+      name: "Top 10",
+      type: "bar",
+      orientation: "h"
+    }
+  ];
+
+    var barLayout = {
+      title: "Top 10 Bacteria Cultures Found",
+      margin: { t: 30, l: 150 }
+    };
+
+Plotly.newPlot("bar", data, barLayout);
   })
-});
-  
-// d3.selectAll("#selDataset").on("change", optionChanged);
+};
 
-// function optionChanged(value){  
-//     // console.log(value);
-//     makeBarChart(value);
-//     bubbleChart(value);
-//     metadata(value);
-//     }
+function init() {
+  var selector = d3.select('#selDataset');
 
-// Use D3 fetch to read the JSON file
-// The data from the JSON file is arbitrarily named importedData as the argument
-d3.json("samples.json").then((importedData) => {
-    console.log(importedData);
-//     var data = importedData;
-  
-//     // Sort the data array using the greekSearchResults value
-//     data.sort(function(a, b) {
-//       return parseFloat(b.sample_values) - parseFloat(a.sample_values);
-//     });
-  
-//     // Slice the first 10 objects for plotting
-//     data = data.slice(0, 10);
-  
-//     // Reverse the array due to Plotly's defaults
-//     data = data.reverse();
-  
-//     // Trace1 for the Greek Data
-//     var trace1 = {
-//       x: data.map(row => row.sample_values),
-//       y: data.map(row => row.otu_ids),
-//       text: data.map(row => row.otu_labels),
-//       name: "Greek",
-//       type: "bar",
-//       orientation: "h"
-//     };
-  
-//     // data
-//     var chartData = [trace1];
-  
-//     // Apply the group bar mode to the layout
-//     var layout = {
-//       title: "Greek gods search results",
-//       margin: {
-//         l: 100,
-//         r: 100,
-//         t: 100,
-//         b: 100
-//       }
-//     };
-  
-//     // Render the plot to the div tag with id "plot"
-//     Plotly.newPlot("bar", chartData, layout);
+  d3.json('samples.json').then((data) => {
+    var names = data.names;
+
+    names.forEach((sample) => {
+      selector.append("option")
+        .text(sample)
+        .property("value", sample);
+    });
+    var initSample = names[0];
+    buildBarchart(initSample);
   });
-  
+};
+
+init();
